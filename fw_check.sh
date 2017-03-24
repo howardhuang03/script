340c67041340c297994cd7f980d34307dad0e129
Howardhuang <howardhuang03@gmail.com>
2017-03-24 15:23:31 +0800

    [Script] Update fw_check.sh
    fw_check.sh [-f] [-s] [-u]
    Usage:
      -f: Enable capital portal function
      -s: Show capital portal related status
      -u: Execute firmware upgrade
    
    Signed-off-by: Howardhuang <howardhuang03@gmail.com>

--- fw_check.sh
+++ fw_check.sh
@@ -2,62 +2,144 @@
 
 . $IPKG_INSTROOT/lib/functions.sh
 
+PROGNAME=$(basename $0)
 realm="$(uci_get system.@system[0].hostname)"
+hwmach="howardhuang@192.168.0.66"
 target=hq.tar.gz
+maindomain=xxxx.com.tw
+mainhost=www.$maindomain
+replacedomain=$maindomain
+testdomain=yyyy.com.tw
+testhost1=wifitest.office.$testdomain
+testhost2=wifitestpc.office.$testdomain
+realhost=$testhost2
+ssid=wifi-test
+sysupgradefw="lede-ar71xx-generic-archer-c7-v2-squashfs-sysupgrade.bin"
+target_msg="4FRee Request Info Page"
 
-function luci_key_change {
-	echo "Chagne LuCI password"
+# Show help
+show_help() {
+	cat <<- EOF
+	$PROGNAME [-f] [-s] [-u]
+	Usage:
+	  -f: Enable capital portal function
+	  -s: Show capital portal related status
+	  -u: Execute firmware upgrade
+	EOF
+}
+
+luci_key_change() {
+	echo "Chagne LuCI password to '123' haha"
 	passwd
 }
 
-function webif_key_change {
-	echo "Change webif password"
-	sed -i 's/xxxx/xxxx/g' /etc/httpd.conf
+webif_key_change() {
+	echo "Change webif password to '123' haha"
+	sed -i 's/xxxxx/xxxxx/g' /etc/httpd.conf
 }
 
-function change_4free_config {
-        echo "Change fourfree host and redirect url"
-	uci set fourfree.settings.redirect_host=xxx.com.tw
-	uci set fourfree.settings.redirect_url=http://xxx.com.tw/site/GW-HQ/
-        uci commit fourfree
+squid_domain_change() {
+	old_domain=$1
+	echo "old_domain: $old_domain"
+	new_domain=$2
+	echo "new_domain: $new_domain"
+	echo "Add test domain"
+	sed -i "s/$old_domain/$new_domain/g" /etc/squid/squid.conf.in
 }
 
-function change_wifi_config {
+change_4free_config() {
+	echo "Change fourfree host and redirect url"
+	uci set fourfree.settings.redirect_host=$realhost
+	uci set fourfree.settings.redirect_url=http://$realhost/site/GW-HQ/
+	uci commit fourfree
+}
+
+change_wifi_config() {
 	echo "Change wifi SSID & password"
-	uci set wireless.4free_5G.ssid=LEDE-99G
-        uci set wireless.4free_5G.encryption=psk2
-        uci set wireless.4free_5G.key=12345678
-	uci set wireless.4free.ssid=LEDE
-        uci set wireless.4free.encryption=psk2
-        uci set wireless.4free.key=12345678
-        uci commit wireless
+	uci set wireless.4free_5G.ssid=$ssid-5G
+	uci set wireless.4free_5G.encryption=psk2
+	uci set wireless.4free_5G.key=12345678
+	uci set wireless.4free.ssid=$ssid
+	uci set wireless.4free.encryption=psk2
+	uci set wireless.4free.key=12345678
+	uci commit wireless
 }
 
-# Enable 4free function
-if [ "$realm" != "hq" ]; then
-	scp howardhuang@192.168.0.66:share/tmp/hq/hq.tar.gz /
+enable_portal() {
+	# Enable capital portal
+	echo "Activating capital portal"
+	cd /
+	scp $hwmach:share/tmp/hq/$target /
 	if [ -f "$target" ]; then
 		tar -zxvf $target
+		rm $target
 	fi
 
 	luci_key_change
 	webif_key_change
+	squid_domain_change $replacedomain $testdomain
 	change_wifi_config
 	change_4free_config
-	echo "Update machine to 4free hq, please reboot the device"
+	echo "Finish update to 4free hq, reboot the device"
 	exit 0
-fi
+}
 
-# Marker checking rule
-echo "Marker checking rule"
-nslookup wifitest.office.pilottv.com.tw
+check_status() {
+	# Marker checking rule
+	echo "Marker checking rule"
+	nslookup $realhost
 
-# ndskeeper checking rule
-echo -e "Run ndskeeper"
-/etc/nodogsplash/ndskeeper.sh
-echo -e "nodogsplash status: "
-cat /tmp/state/nodogsplash
+	# ndskeeper checking rule
+	echo -e "\n\nndskeeper checking rule"
+	hsinfo="http://$realhost/hsinfo"
+	wget -t 1 -T 15 -q -O- $hsinfo | grep '4FRee Request Info Page'
+	#echo -e "Run ndskeeper"
+	#/etc/nodogsplash/ndskeeper.sh
+	echo -e "\nnodogsplash status: "
+	cat /tmp/state/nodogsplash
 
-# Print iptables mangle's table
-echo -e "\n\nPrint out mangle table"
-iptables -t mangle -L
+	# offline-notifier checking
+	echo -e "\noffline-notifier status: "
+	cat /tmp/state/offline-notifier
+
+	# Print iptables mangle's table
+	echo -e "\n\niptalbes MANGLE rule"
+	iptables -t mangle -L
+}
+
+do_sysupgrade() {
+	local target=$sysupgradefw
+
+	if [ -f "/tmp/$target" ]; then
+		echo "Download sysupgrade firmware"
+		scp $hwmach:share/$target /tmp
+	fi
+
+	echo "Performing sysupgrade with $target"
+	sysupgrade -v $target
+}
+
+main() {
+	while getopts :fsu flag
+	do
+		case $flag in
+		f)
+			enable_portal
+			;;
+		s)
+			check_status
+			;;
+		u)
+			do_sysupgrade
+			;;
+		\?)
+			show_help
+			exit 0
+			;;
+		esac
+	done
+}
+
+# Start main process
+[ -z "$1" ] && main -h
+main "$@"
